064503099dd00c775239a2c2420b0b4b40461ae6
Lara Toledo <110473452+laratoledom@users.noreply.github.com>
2022-08-03 23:56:50 -0300

    Update codigo04.py

--- codigo04.py
+++ codigo04.py
@@ -1,1 +1,20 @@
-#Colocar o código aqui
+n = int(input())
+for i in range(n):
+    x = int(input())
+    pares = 0
+    impares = 0
+    maior = x
+    print("Valor inicial:", x)
+    while x != 1:
+        if x > maior:
+            maior = x
+        if (x % 2) == 0:
+            pares = pares + 1
+            x = x/2
+        else:
+            impares = impares + 1
+            x = (x*3) + 1
+    impares = impares + 1
+    print("Numeros Pares:", pares)
+    print("Numeros Impares:", impares)
+    print("Maior Numero:", int(maior))
